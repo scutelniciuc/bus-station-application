a673a16ac54682be8ce451e560ce8ab471a3ebac
scutelniciuc <scutelniciuc.dev@gmail.com>
2024-04-01 11:40:08 +0100

    added Driver model

--- backend/bus_station/models.py
+++ backend/bus_station/models.py
@@ -1,5 +1,19 @@
+import uuid
+
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinValueValidator
 
 class User(AbstractUser):
     pass
+
+class Driver(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    first_name = models.CharField(max_length=128)
+    last_name = models.CharField(max_length=128)
+    birth_date = models.DateField(blank=True, null=True)
+    years_of_experience = models.IntegerField(validators=[MinValueValidator(0)])
+    date_joined = models.DateField()
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
