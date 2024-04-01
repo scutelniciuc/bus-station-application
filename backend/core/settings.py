c00ff7f1b70ace13c4ac2b965a3412f02ee98007
scutelniciuc <scutelniciuc.dev@gmail.com>
2024-04-01 10:11:08 +0100

    override User model

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "bus_station.apps.Bus_stationConfig"
+    'bus_station',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +122,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = "bus_station.User"
