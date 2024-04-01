30058e52dfb24007d9877f4a2077ee50f3d316c6
scutelniciuc <scutelniciuc.dev@gmail.com>
2024-04-01 12:03:51 +0100

    added Bus model

---
+++ backend/bus_station/constants.py
@@ -0,0 +1,6 @@
+BUS_STATUS_CHOICES = [
+        ('on_platform', 'On platform'),
+        ('arriving', 'Arriving'),
+        ('leaving', 'Leaving'),
+        ('left', 'Left'),
+]
--- backend/bus_station/models.py
+++ backend/bus_station/models.py
@@ -2,7 +2,8 @@ import uuid
 
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.core.validators import MinValueValidator
+from django.core.validators import MinValueValidator, RegexValidator
+from .constants import BUS_STATUS_CHOICES
 
 class User(AbstractUser):
     pass
@@ -17,3 +18,29 @@ class Driver(models.Model):
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
+
+class Bus(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    registration_plate = models.CharField(
+        max_length=8,
+        unique=True,
+        validators=[
+            RegexValidator(
+                regex=r'^[A-Z0-9]{2}-[A-Z0-9]{2}-[A-Z0-9]{2}$',
+                message='Format must be XX-XX-XX',
+                code='invalid_registration_plate'
+            )
+        ]
+    )
+    driver = models.ForeignKey(Driver, on_delete=models.CASCADE)
+    end_destination = models.CharField(max_length=128)
+    time_of_departure = models.DateTimeField()
+    max_seats = models.IntegerField(validators=[MinValueValidator(8)])
+    seats_available = models.IntegerField(validators=[MinValueValidator(0)])
+    status = models.CharField(max_length=16, choices=BUS_STATUS_CHOICES)
+
+    class Meta:
+        verbose_name_plural = "Buses"
+
+    def __str__(self):
+        return f'Destination: {self.end_destination} - Departure: {self.time_of_departure.strftime("%H:%M %d-%m-%Y")}'
