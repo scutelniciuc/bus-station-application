deebd4c5bffedbdda6ed5d9b613d7e927e0f381b
scutelniciuc <scutelniciuc.dev@gmail.com>
2024-04-02 18:55:49 +0100

    populate table with buses

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -48,11 +48,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'bus_station',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -133,3 +135,5 @@ AUTH_USER_MODEL = "bus_station.User"
 # Celery settings
 
 CELERY_BROKER_URL=env("CELERY_BROKER_URL")
+
+CORS_ALLOW_ALL_ORIGINS = True
